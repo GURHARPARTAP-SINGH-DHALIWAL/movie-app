c6971015afdfec468352df5427c6ee0bf4f98ddb
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-10-06 22:38:24 +0530

    Add UI, Reducers, Actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,34 @@
+import {data} from '../data';
+import NavBar from './NavBar';
+import MovieCard from './MovieCard';
 
 
 function App() {
   return (
     <div className="App">
-      My App
+      
+      <NavBar />
+      <div className="main">
+
+          <div className="tabs">
+              <div className="tab">Movies</div>
+              <div className="tab">Favourites</div>
+          </div>
+
+          <div className="list">
+
+              {
+                data.map((item)=>{
+                  console.log(item);
+                  return  <MovieCard movie={item} />
+                })
+              }
+
+          </div>
+
+      </div>
+
+
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class NavBar extends React.Component {
+    render() { 
+        return(
+            <div className="nav">
+                <div className="search-container">
+                    <input type="text"/>
+                    <button id="search-btn">Search</button>
+                </div>
+            </div>
+        );
+    }
+}
+ 
+export default NavBar;
